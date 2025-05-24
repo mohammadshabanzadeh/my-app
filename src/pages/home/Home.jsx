b39c8f2be41de5c2824ae499bc24b198b433743a
Mohammad <mh.Shabanzadeh1@gmail.com>
2025-05-24 18:40:21 +0330

    create nav

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../component/navbar/Navbar";
+function Home() {
+  return (
+    <div>
+      <Navbar title="ممد بلاگ" />
+      <div className="container">
+        <h1>salam</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
