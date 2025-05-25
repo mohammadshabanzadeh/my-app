fba97578bfd698f50cc45da462ee375025b06637
Mohammad <mh.Shabanzadeh1@gmail.com>
2025-05-25 10:20:05 +0330

    first today

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,19 +1,19 @@
-import styled from "./navbar.module.css"
-function Navbar(props){
-    return(
-        <div className={styled.headerWrapper}>
-        <div className="container">
+import styled from "./navbar.module.css";
+function Navbar(props) {
+  return (
+    <div className={styled.headerWrapper}>
+      <div className="container">
         <div className={styled.header}>
-            <h3>{props.title}</h3>
-            <ul>
-                <li>لیست مقالات</li>
-                <li>ساخت مقاله</li>
-                <li>درباره</li>
-            </ul>
+          <h3>{props.title}</h3>
+          <ul>
+            <li>لیست مقالات</li>
+            <li>ساخت مقاله</li>
+            <li>درباره</li>
+          </ul>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
